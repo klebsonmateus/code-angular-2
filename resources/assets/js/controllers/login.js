246abe52336ba5eaa4c1cca0656f9fdce840f1ae
Klebson Mateus <klebsonbm@gmail.com>
2016-05-28 15:10:51 -0300

    Mostrando erros de login na tela

--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -6,12 +6,18 @@ angular.module('app.controllers')
 		password: ''
 	};
 
+	$scope.error = {
+		message: '',
+		error: false
+	};
+
 	$scope.login = function() {
 		if($scope.form.$valid) {
 			OAuth.getAccessToken($scope.user).then( function() {
 				$location.path('home');
-			}, function() {
-				alert('Login Inválido');
+			}, function(data) {
+				$scope.error.error = true;
+				$scope.error.message = data.data.error_description;
 			});
 		}
 	};
