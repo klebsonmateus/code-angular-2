dcf1c67da16536737b1f6aa7879a10578018b53c
Klebson Mateus <klebsonbm@gmail.com>
2016-06-02 17:59:57 -0300

    Iniciando com edição de project

--- public/build/js/controllers/project/projectEdit.js
+++ public/build/js/controllers/project/projectEdit.js
@@ -1,21 +1,16 @@
 angular.module('app.controllers')
 	.controller('ProjectEditController', 
-		['$scope','$routeParams','$location', 'ProjectNote', 
-	function($scope, $routeParams , $location,  ProjectNote) {
-	$scope.projectNote = ProjectNote.get({
-		id: $routeParams.id,
-		idNote: $routeParams.idNote
-	});
-
-	$scope.save = function () {
+		['$scope', '$routeParams', '$location', '$cookies', 'Project', 'Client','appConfig', 
+	function($scope, $routeParams, $location,  $cookies, Project, Client, appConfig) {
+	$scope.project = Project.get({id: $routeParams.id});
+	$scope.clients = Client.query();
+	$scope.status = appConfig.project.status;
+	$scope.save = function(){
 		if($scope.form.$valid){
-			ProjectNote.update({
-				id: $scope.projectNote.project_id , idNote: $scope.projectNote.id}, 
-				$scope.projectNote,
-				function() {
-				$location.path('/project/' + $routeParams.id + '/notes');
+			$scope.project.owner_id = $cookies.getObject('user').id;
+			Project.update({id: $scope.project.id}, $scope.project, function(){
+				$location.path('/projects');
 			});
-			
 		}
 	}
 
