508127e2303e28938fd8107e71a0dfaf7ccbcd53
Klebson Mateus <klebsonbm@gmail.com>
2016-06-02 18:46:32 -0300

    Transformando due_date

--- public/build/js/services/project.js
+++ public/build/js/services/project.js
@@ -12,6 +12,17 @@ angular.module('app.services')
 						return data;
 					}
 			},
+			get: {
+				method: 'GET',
+					transformResponse: function(data, headers){
+						var o = appConfig.utils.transformResponse(data, headers);
+						if(angular.isObject(o) && o.hasOwnProperty('due_date')){
+							var arrayDate = o.due_date.split('-');
+							o.due_date = new Date(arrayDate[0], arrayDate[1], arrayDate[2]);
+						}
+						return o;
+					}
+			},
 			update: {
 				method: 'PUT'
 			}
