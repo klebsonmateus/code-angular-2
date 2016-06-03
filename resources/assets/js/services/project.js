d7965d417e44a638c9f9066266d69ee2a4a99068
Klebson Mateus <klebsonbm@gmail.com>
2016-06-02 23:09:43 -0300

    Editando project

--- resources/assets/js/services/project.js
+++ resources/assets/js/services/project.js
@@ -1,16 +1,17 @@
 angular.module('app.services')
 .service('Project', ['$resource', '$filter', '$httpParamSerializer', 'appConfig', 
 	function($resource, $filter, $httpParamSerializer, appConfig) {
-		return $resource(appConfig.baseUrl + '/project/:id' , {id: '@id'},{
-			save: {
-				method: 'POST',
-					transformRequest: function (data) {
-						if(angular.isObject(data) && data.hasOwnProperty('due_date')){
+		function transformData(data) {
+			if(angular.isObject(data) && data.hasOwnProperty('due_date')){
 							data.due_date = $filter('date')(data.due_date, 'yyyy-MM-dd');
 							return $httpParamSerializer(data);
 						}
 						return data;
-					}
+		};
+		return $resource(appConfig.baseUrl + '/project/:id' , {id: '@id'},{
+			save: {
+				method: 'POST',
+					transformRequest: transformData 
 			},
 			get: {
 				method: 'GET',
@@ -24,7 +25,9 @@ angular.module('app.services')
 					}
 			},
 			update: {
-				method: 'PUT'
+				method: 'PUT',
+					transformRequest: transformData 
+
 			}
 		});
 	}]);
