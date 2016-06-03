0d88af73a44478f51486aa4fd24112ce22c721d5
Klebson Mateus <klebsonbm@gmail.com>
2016-06-03 17:34:24 -0300

    Typeahead e ajax

--- resources/assets/js/controllers/project/projectNew.js
+++ resources/assets/js/controllers/project/projectNew.js
@@ -25,4 +25,11 @@ angular.module('app.controllers')
 		return '';
 	};
 
+	$scope.getClients = function (name) {
+		return Client.query({
+			search: name,
+			searchFields: 'name:like'
+		}).$promise;
+	}
+
 }]);
