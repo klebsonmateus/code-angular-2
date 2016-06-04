7c3a80578f4f5894035ee8aa31a61840b417eb85
Klebson Mateus <klebsonbm@gmail.com>
2016-06-04 20:14:49 -0300

    Configurando rotas de project file

---
+++ public/build/js/controllers/project-note - Copia/projectNoteEdit.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers')
+	.controller('ProjectNoteEditController', 
+		['$scope','$routeParams','$location', 'ProjectNote', 
+	function($scope, $routeParams , $location,  ProjectNote) {
+	$scope.projectNote = ProjectNote.get({
+		id: $routeParams.id,
+		idNote: $routeParams.idNote
+	});
+
+	$scope.save = function () {
+		if($scope.form.$valid){
+			ProjectNote.update({
+				id: $scope.projectNote.project_id , idNote: $scope.projectNote.id}, 
+				$scope.projectNote,
+				function() {
+				$location.path('/project/' + $routeParams.id + '/notes');
+			});
+			
+		}
+	}
+
+}]);
---
+++ resources/assets/js/controllers/project-file/projectFileRemove.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers')
+	.controller('ProjectNoteRemoveController', 
+		['$scope','$routeParams','$location', 'ProjectNote', 
+	function($scope, $routeParams, $location,  ProjectNote) {
+	$scope.projectNote = ProjectNote.get({
+		id: $routeParams.id,
+		idNote: $routeParams.idNote
+	});
+
+	$scope.remove = function () {
+		$scope.projectNote.$delete({
+			id: $scope.projectNote.project_id , idNote: $scope.projectNote.id
+		}).then(function(){
+			$location.path('/project/' + $routeParams.id + '/notes');
+		});
+	}
+
+}]);
