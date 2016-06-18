33fac9769f52d186ae8b7c9778cc95dd0fe00228
Klebson Mateus <klebsonbm@gmail.com>
2016-06-17 23:15:39 -0300

    Listagem de tasks

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -110,6 +110,11 @@
 		<script src="{{asset('build/js/controllers/project-file/projectFileEdit.js')}}"></script>
 		<script src="{{asset('build/js/controllers/project-file/projectFileRemove.js')}}"></script>
 
+		<script src="{{asset('build/js/controllers/project-task/projectTaskList.js')}}"></script>
+		<script src="{{asset('build/js/controllers/project-task/projectTaskNew.js')}}"></script>
+		<script src="{{asset('build/js/controllers/project-task/projectTaskEdit.js')}}"></script>
+		<script src="{{asset('build/js/controllers/project-task/projectTaskRemove.js')}}"></script>
+
 		<!-- DIRECTIVES !-->
 		<script src="{{asset('build/js/directives/projectFileDownload.js')}}"></script>
 
