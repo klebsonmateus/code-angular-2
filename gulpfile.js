17c9e1852efe7562c9d25ba017afe368551666f3
Klebson Mateus <klebsonbm@gmail.com>
2016-07-15 12:54:55 -0300

    Instalando biblioteca de paginação

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ config.vendor_path_js = [
 	config.bower_path + '/angular-oauth2/dist/angular-oauth2.min.js',
 	config.bower_path + '/ng-file-upload/ng-file-upload.min.js',
 	config.bower_path + '/angular-http-auth/src/http-auth-interceptor.js',
+	config.bower_path + '/angularUtils-pagination/dirPagination.js',
 
 
 ]
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('app',[
 	'ngRoute', 'angular-oauth2', 'app.controllers', 
 	'app.services', 'app.filters', 'app.directives',
 	'ui.bootstrap.typeahead', 'ui.bootstrap.datepicker', 
-	'ui.bootstrap.tpls', 'ui.bootstrap.modal', 'ngFileUpload', 'http-auth-interceptor'
+	'ui.bootstrap.tpls', 'ui.bootstrap.modal', 'ngFileUpload', 'http-auth-interceptor','angularUtils.directives.dirPagination'
 	]);
 
 angular.module('app.controllers', ['ngMessages', 'angular-oauth2']);
