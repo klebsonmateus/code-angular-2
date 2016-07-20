f878e6aeec5d239e38ce73c289c094945ed1bcac
Klebson Mateus <klebsonbm@gmail.com>
2016-07-19 21:13:44 -0300

    Terminando dashboard de project

--- app/Transformers/ProjectTransformer.php
+++ app/Transformers/ProjectTransformer.php
@@ -7,7 +7,7 @@ use League\Fractal\TransformerAbstract;
 
 class ProjectTransformer extends TransformerAbstract
 {
-    protected $defaultIncludes = ['members','client'];
+    protected $defaultIncludes = ['members','notes', 'tasks', 'files', 'client'];
 
     public function transform(Project $project)
     {
@@ -20,7 +20,9 @@ class ProjectTransformer extends TransformerAbstract
             'progress' => (int) $project->progress,
             'status' => $project->status,
             'due_date' => $project->due_date,
-            'is_member' => $project->owner_id != \Authorizer::getResourceOwnerId()
+            'is_member' => $project->owner_id != \Authorizer::getResourceOwnerId(),
+            'tasks_count' => $project->tasks->count(),
+            'tasks_opened' => $this->countTasksOpened($project)
         ];
     }
 
@@ -35,6 +37,31 @@ class ProjectTransformer extends TransformerAbstract
             return $this->item($project->client, new ClientTransformer());
         }
         return null;
+    }
+
+    public function includeNotes(Project $project)
+    {
+        return $this->collection($project->notes, new ProjectNoteTransformer());
+    }
+
+    public function includeFiles(Project $project)
+    {
+        return $this->collection($project->files, new ProjectFileTransformer());
+    }
+
+    public function includeTasks(Project $project)
+    {
+        return $this->collection($project->tasks, new ProjectTaskTransformer());
+    }
+
+    public function countTasksOpened(Project $project){
+        $count = 0;
+        foreach($project->tasks as $o){
+            if($o->status == 1) {
+                $count++;
+            }
+        }
+        return $count;
     }
 
 }
