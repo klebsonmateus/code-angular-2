7ec2bcf02735cbcc4f9f79ec6956ce9463f65055
Klebson Mateus <klebsonbm@gmail.com>
2016-07-24 19:08:32 -0300

    Instalando pusher-angular

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('app',[
 	'app.services', 'app.filters', 'app.directives',
 	'ui.bootstrap.typeahead', 'ui.bootstrap.datepicker', 
 	'ui.bootstrap.tpls', 'ui.bootstrap.modal', 'ngFileUpload', 'http-auth-interceptor',
-	'angularUtils.directives.dirPagination', 'ui.bootstrap.dropdown',
+	'angularUtils.directives.dirPagination', 'ui.bootstrap.dropdown','pusher-angular'
 	]);
 
 angular.module('app.controllers', ['ngMessages', 'angular-oauth2']);
